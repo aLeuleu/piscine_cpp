36af66dca7ae12d5575287b35705957ccda950ae
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-08-24 22:31:56 +0200

    _

--- unitary_tests.sh
+++ unitary_tests.sh
@@ -2,28 +2,15 @@
 
 
 cd ./Module01/ex00
-echo
-echo
-make
-if [ $? -ne 0 ]; then
-    echo "Compilation failed"
-    exit 1
-fi
-valgrind --leak-check=full ./Zombie 1>/dev/null 2>/dev/null
-if [ $($?) -ne 0 ]; then
-    echo "Leaks"
-    exit 1
-fi
-echo "01_00 OK"
 
-cd ../ex01
-make --silent
-if [ $? -ne 0 ]; then
-    echo "Compilation failed"
-    exit 1
-fi
-if [ $(valgrind --leak-check=full ./ZombieHorde 1>/dev/null) -ne 0 ]; then
-    echo "Leaks"
-    exit 1
-fi
-echo "01_01 OK"
+for i in {00..02}
+do
+	cd ../ex$i
+	echo "01_$i :"
+	make leaks
+done
+
+
+
+
+
