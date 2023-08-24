116408209743f26f62fe01f7511b695ef8411a9c
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-08-24 22:21:19 +0200

    _

--- unitary_tests.sh
+++ unitary_tests.sh
@@ -7,7 +7,7 @@ if [ $? -ne 0 ]; then
     echo "Compilation failed"
     exit 1
 fi
-if [ $(valgrind --leak-check=full ./Zombie) -ne 0 ]; then
+if [ $(valgrind --leak-check=full ./Zombie 1>/dev/null) -ne 0 ]; then
     echo "Leaks"
     exit 1
 fi
@@ -19,7 +19,7 @@ if [ $? -ne 0 ]; then
     echo "Compilation failed"
     exit 1
 fi
-if [ $(valgrind --leak-check=full ./ZombieHorde) -ne 0 ]; then
+if [ $(valgrind --leak-check=full ./ZombieHorde 1>/dev/null) -ne 0 ]; then
     echo "Leaks"
     exit 1
 fi
